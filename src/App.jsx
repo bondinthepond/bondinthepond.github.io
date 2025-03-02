2dbc0dbb7f588e63241102d45f1be636cfade158
Aditya Mallela <adityamallela.1988@gmail.com>
2025-03-02 22:30:08 +0530

    Handling deployment issues - added dist folder

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./styles.css";
+import Header from "./components/CoreComponents/1-Header.jsx";
+import About from "./components/CoreComponents/2-About.jsx";
+import Experience from "./components/CoreComponents/5-Experience.jsx";
+import Contact from "./components/CoreComponents/6-Contact.jsx";
+import Download from "./components/CoreComponents/7-Download.jsx";
+import ManagementSkills from "./components/CoreComponents/4-ManagementSkills.jsx";
+import TechnicalSkills from "./components/CoreComponents/3-TechnicalSkills.jsx";
+
+function App() {
+	return (
+		<div>
+			<Header/>
+			<div className = "container">
+				<About/>
+				<TechnicalSkills/>
+				<ManagementSkills/>
+				<Experience/>
+				<Contact/>
+				<Download/>
+			</div>
+		</div>
+	);
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import {createRoot} from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
+
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<App/>
