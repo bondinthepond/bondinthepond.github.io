df4ab0abcc2178c8136185cba2fe3d4e0d9b732b
Aditya Mallela <adityamallela.1988@gmail.com>
2025-03-04 14:33:42 +0530

    Fixing download functionality

--- src/components/CoreComponents/7-Download.jsx
+++ src/components/CoreComponents/7-Download.jsx
@@ -5,7 +5,7 @@ function Download() {
 		<section id = "download" class = "download">
 			<h2>Resume</h2>
 			<p class = "download">Click to {" "}
-				<a href = "../../resume/Aditya_Resume.pdf" download class = "download-btn">download my resume</a></p>
+				<a href = "src/resume/Aditya_Resume.pdf" download class = "download-btn">download my resume</a></p>
 		</section>
 	);
 }
